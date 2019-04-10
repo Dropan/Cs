aabe6ca19c429b37b5ba11fb9937af3bbccf1e8f
Aleksey Novikov <anovikov@garda.local>
2019-04-10 11:04:23 +0300

    New

---
+++ Levenshtein/Levenshtein/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace levenshtein
+{
+    class Levenshtein
+    {
+        public static int AlgLev(string[] str1, string[] str2)
+        {
+            int l = str1.Length, t = str2.Length, v = 0;
+            int[,] a = new int[l + 1, t + 1];
+            if (l == 0 || t == 0) return Math.Max(l, t);
+            for (int i = 0; i < l + 1; i++)
+            {
+                a[i, 0] = i;
+                for (int j = 0; j < t + 1; j++)
+                {
+                    a[0, j] = j;
+                }
+            }
+
+            for (int i = -1; ++i < l;)
+                for (int j = -1; ++j < t;)
+                {
+                    if (str1[i] != str2[j]) v = 1;
+                    else v = 0;
+                    a[(i *= 1) + 1, (j *= 1) + 1] = Math.Min(Math.Min(a[i, j + 1] + 1, a[i + 1, j] + 1), a[i, j] + v);
+                }
+            Console.WriteLine("\nMatrix:\n");
+            for (int i = 0; i < l + 1; i++)
+            {
+                for (int j = 0; j < t + 1; j++)
+                {
+                    Console.Write(a[i, j]);
+                }
+                Console.Write('\n');
+            }
+
+            Console.WriteLine("\nDistance: " + a[l, t]);
+            return a[l, t];
+        }
+
+        class Mainer
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hi");
+                Console.WriteLine("");
+                Console.WriteLine("test1 AlgLev");
+                string[] str1 = { "z", "o", "n", "n", "e", "v", "e", "l", "d" }, str2 = { "s", "o", "m", "m", "e", "v", "e", "l", "d" };
+                int ind1 = AlgLev(str1, str2);
+                if (ind1 == 3)
+                {
+                    Console.WriteLine("True");
+                }
+                else Console.WriteLine("False");
+                Console.WriteLine("");
+                Console.WriteLine("test2 AlgLev");
+                string[] str3 = { "т", "о", "с", "т" }, str4 = { "т", "е", "к", "с", "т" };
+                int ind2 = AlgLev(str3, str4);
+                if (ind2 == 2)
+                {
+                    Console.WriteLine("True");
+                }
+                else Console.WriteLine("False");
+                Console.WriteLine("");
+                Console.WriteLine("test3 AlgLev");
+                string[] str5 = { "a", "r", "o", "t" }, str6 = { "s", "g", "j", "k" };
+                int ind3 = AlgLev(str5, str6);
+                if (ind3 == 4)
+                {
+                    Console.WriteLine("True");
+                }
+                else Console.WriteLine("False");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ isbn/isbn/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isbn
+{
+    class Isbn
+    {
+        public static bool Validate(string[] str)
+        {
+            if (str.Length == 10)
+            {
+                if (isbn10(str) == true) return true;
+                else return false;
+            }
+            else if (str.Length == 13)
+            {
+                if (isbn13(str) == true) return true;
+                else return false;
+            }
+            else return false;
+        }
+        public static bool isbn10(string[] str)
+        {
+            int sumpr = 0;
+            for (int i = 0; i <= str.Length - 2; i++)
+            {
+                //if (str[i] == "-") strk[i] = str.Remove(i, 1);
+                sumpr += int.Parse(str[i]) * (str.Length - i);
+            }
+            if (str[str.Length - 1] == "x" && str[str.Length - 1] == "X") str[str.Length - 1] = "10";
+
+            if ((sumpr + (int.Parse(str[str.Length - 1]))) % 11 == 0) return true;
+            else return false;
+        }
+        public static bool isbn13(string[] str)
+        {
+            if (str[2] != "8" && str[2] != "9") return false;
+            int sumpr = 0;
+            string[] strk = new string[str.Length];
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                //if (str[i] == "-") strk[i] = str.Remove(i, 1);
+                strk[i] = str[i];
+            }
+            strk[str.Length - 1] = "0";
+            for (int j = 0; j < str.Length - 1; j++)
+            {
+                if (j % 2 == 0) sumpr += int.Parse(strk[j]);
+                else sumpr += int.Parse(strk[j]) * 3;
+            }
+            if (int.Parse(str[str.Length - 1]) == (10 - (sumpr % 10)) || int.Parse(str[str.Length - 1]) == 10) return true;
+            else return false;
+        }
+
+        class Mainer
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hi");
+                Console.WriteLine("");
+                Console.WriteLine("test1 isbn 10");
+                string[] str10 = {"2","2","6","6","1","1","1","5","6","6"};
+                bool ind1 = isbn10(str10);
+                if (ind1 == true)
+                {
+                    Console.WriteLine("True");
+                }
+                else Console.WriteLine("False");
+                Console.WriteLine("");
+                Console.WriteLine("test2 isbn 13");
+                string[] str13 = { "9", "7", "8", "2", "1", "2", "3", "4", "5", "6", "8", "0", "3" };
+                bool ind2 = isbn13(str13);
+                if (ind2 == true)
+                {
+                    Console.WriteLine("True");
+                }
+                else Console.WriteLine("False");
+                Console.WriteLine("");
+                Console.WriteLine("test3 Validate10");
+                string[] str1 = { "2", "2", "6", "6", "1", "1", "1", "5", "6", "6" };
+                bool ind3 = Validate(str1);
+                if (ind3 == true)
+                {
+                    Console.WriteLine("True");
+                }
+                else Console.WriteLine("False");
+                Console.WriteLine("");
+                Console.WriteLine("test4 Validate13");
+                string[] str2 = { "9", "7", "8", "2", "1", "2", "3", "4", "5", "6", "8", "0", "3" };
+                bool ind4 = Validate(str2);
+                if (ind4 == true)
+                {
+                    Console.WriteLine("True");
+                }
+                else Console.WriteLine("False");
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
